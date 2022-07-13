a98987db5e1e425a7bcb276e962fb7265594223a
Angelita Gozaly <g-angelita-build2022@mercari-partner.com>
2022-07-14 00:51:34 +0900

    User name added

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import random
 numOfHead = 0
 numOfTail = 0
 
+userName = input('Who are you?\n')
+print("Hello, ", userName, "!")
+
 print("Tossing a coin...")
 for round in range(3):
     coinToss = random.randint(1, 2)
